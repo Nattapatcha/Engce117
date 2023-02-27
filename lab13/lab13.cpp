dbbabf2df2fb45095b85b5cbedcfdc000794d9ff
Nattapat <fixnattapat@gmail.com>
2023-02-27 11:36:28 +0700

    lab13

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
 int *KnapsackDP( int *w, int *v, int n, int wx ) ;
 
-int main() {
-int n = 5 , wx = 11 ;
-int w[ 5 ] = { 1, 2, 5, 6, 7 } ;
-int v[ 5 ] = { 1, 6, 18, 22, 28 } ;
-int *x ;
-x = KnapsackDP( w, v, n, wx ) ;
-for( int i = 0 ; i < n ; i++ ) printf( "%d ", x[ i ] ) ;
-return 0 ;
-}//end function
+int main(){
+    int n = 5 , wx = 11 ;
+    int w[ 5 ] = { 1, 2, 5, 6, 7 } ;
+    int v[ 5 ] = { 1, 6, 18, 22, 28 } ;
+    int *x ;
+    x = KnapsackDP( w, v, n, wx ) ;
+    for( int i = 0 ; i < n ; i++ ) printf( "%d ", x[ i ] ) ;
+    return 0 ;
+}
+
 int *KnapsackDP(int *w, int *v, int n, int wx) {
     int i, j;
     int dp[n + 1][wx + 1];
@@ -49,4 +50,3 @@ int *KnapsackDP(int *w, int *v, int n, int wx) {
 
     return x;
 }// end function
-
